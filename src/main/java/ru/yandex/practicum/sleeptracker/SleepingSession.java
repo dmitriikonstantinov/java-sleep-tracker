92583f9abd9ec5bb8a65f82a727eb8a2826280a9
Dmitrii Konstantinov <dimonkent61@yandex.ru>
2026-05-13 15:52:15 +0300

    fix checkStyle класс SleepingSession

--- src/main/java/ru/yandex/practicum/sleeptracker/SleepingSession.java
+++ src/main/java/ru/yandex/practicum/sleeptracker/SleepingSession.java
@@ -16,7 +16,15 @@ public class SleepingSession {
         this.quality = quality;
     }
 
-    public LocalDateTime getStart() { return start; }
-    public LocalDateTime getEnd() { return end; }
-    public Quality getQuality() { return quality; }
+    public LocalDateTime getStart() {
+        return start;
+    }
+
+    public LocalDateTime getEnd() {
+        return end;
+    }
+
+    public Quality getQuality() {
+        return quality;
+    }
 }
